9b4f57a1960a61ebedca04a844fe265c02835874
JacoboJacome <jacobojacome2@gmail.com>
2022-07-05 21:11:42 -0500

    clear code

--- app.js
+++ app.js
@@ -20,36 +20,3 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", email);
-
-// app.post(
-//   "/api/v1/send_email",
-//   catchAsync(async (req, res, next) => {
-//     const { from, to, subject, text } = req.body;
-//     console.log(to);
-//     const transporter = nodemailer.createTransport({
-//       host: "smtp.gmail.com",
-//       post: 465,
-//       secure: true,
-//       auth: {
-//         user: "jacobojacome@gmail.com",
-//         pass: "ewrphfhripwzmmmk",
-//       },
-//     });
-
-//     const mailOptions = {
-//       from,
-//       to,
-//       subject,
-//       text,
-//     };
-
-//     transporter.sendMail(mailOptions, (err, info) => {
-//       if (err) {
-//         res.status(500).send(err.message);
-//       } else {
-//         console.log("email enviado");
-//         res.status(200).json(req.body);
-//       }
-//     });
-//   })
-// );
